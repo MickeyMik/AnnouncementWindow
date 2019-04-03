b4e0a0b71a17c49f002db987391ae9c95977b5aa
Samuel D <samrenfou@hotmail.com>
2019-04-03 12:01:28 +0200

    Add icon section in configuration

--- Config.py
+++ Config.py
@@ -94,6 +94,11 @@ class config(object):
                     if len(fg_bg)==1:
                         fg_bg.append(self.default_bg)
                     self.word_color_dict[color_name]=fg_bg
+            self.icons_word_dict={}
+            for (icon_name,icon_words) in self.parser.items("Icons"):
+                words_list=icon_words.split()
+                for word in words_list:
+                    self.icons_word_dict[word]=icon_name
 
     def save(self):
         with open(self.filepath, 'w') as fi:
